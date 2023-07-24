d34caecfcaa86bd55e2985d6f301a1a7a4a8fd0b
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-07-24 13:49:49 +0000

    solved lab31

--- snacks/models.py
+++ snacks/models.py
@@ -9,5 +9,6 @@ class Snack(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
     def __str__(self):
         return self.title 
